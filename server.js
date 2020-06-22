456f495d5dcb7e9e9fc70322d4bd3de7df8a2cf3
OGI_codes <ognjen495@gmail.com>
2020-06-22 19:25:45 +0200

    Cors added

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 const dburl = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/RacunkoApp";
 const routes = require('./routes/routes');
+var cors = require('cors')
 
 const port = process.env.PORT || 3000
 
 const app = express()
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
